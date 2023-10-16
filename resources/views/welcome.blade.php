dbb141d748954588544a9472a8e1969c6f533df5
rahulalam31 <gaberialcramern31@outlook.com>
2023-10-16 19:48:31 +0530

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
         <i class="checkmark">✓</i>
       </div>
         <h1>Success</h1>
-        <p>We received your purchase request;<br/> we'll be in touch shortly!</p>
+        <p>You have been registered successfully.</p>
       </div>
     </body>
 </html>
